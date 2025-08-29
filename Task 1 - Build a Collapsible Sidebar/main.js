d35b88caca6b5a1d35950d3a1064c65edd9d0048
SkillSageDev <147807207+SkillSageDev@users.noreply.github.com>
2025-08-30 01:08:16 +0300

    feat: add media query, animation, nerd icons
    - optimzed arrow toggle button code
    - used nerd font icons
    - made pages clickable with color highlighting
    - added media query for mobile phones
    - collapseSidebar function can get better!

--- Task 1 - Build a Collapsible Sidebar/main.js
+++ Task 1 - Build a Collapsible Sidebar/main.js
@@ -1,48 +1,61 @@
-function changeLinksItemColor(){
-   let linksItem = document.getElementsByClassName("sidebar__links-item"); // get all links (<li></li>) in side bar
-    for(let item of linksItem){ // for each link (<li></li>)
-        let itemIcon = item.children.item(0); // get the icon
-        item.addEventListener("mouseover", e => { // when mouse move to the link change color
-            item.style.backgroundColor = "#e0e7ff"
-            itemIcon.style.color = "#537fe7";
-        });
-        item.addEventListener("mouseout", e => { // when mouse move out return the color to normal
-            item.style.backgroundColor = ""
-            itemIcon.style.color = "";
-        });
-    }
+function changeLinksItemColor() {
+  let linksItem = document.getElementsByClassName("sidebar__links-item"); // get all links (<li></li>) in side bar
+  for (let item of linksItem) { // for each link (<li></li>)
+    let itemIcon = item.children.item(0); // get the icon
+    // when mouse move to the link change color
+    item.addEventListener("mouseover", e => {
+      item.style.backgroundColor = "#e0e7ff"
+      itemIcon.style.color = "#537fe7";
+    });
+    // when mouse move out return the color to normal
+    item.addEventListener("mouseout", e => {
+      item.style.backgroundColor = ""
+      itemIcon.style.color = "";
+    });
+  }
 }
 
-function toggleSidebar(){
-    let arrows = document.querySelector(".sidebar__arrow-wrapper"); // select the div that has the arrows in it
-    for(let arrow of arrows.children){ // get the actual arrow inside the div
-        arrows.addEventListener("click", e => { // when I click on the div toggle the hidden class for each arrow inside the div
-            arrow.classList.contains("hidden")? arrow.classList.remove("hidden") : arrow.classList.add("hidden");
-        })
-    }
+function rotateSidebarArrow() {
+  let arrow = document.querySelector(".sidebar__arrow"); // 
+  arrow.addEventListener("click", e => {
+    e.target.classList.toggle("rotate");
+  });
 }
 
-function collapseSidebar(){
-    let arrows = document.querySelector(".sidebar__arrow-wrapper");
-    let itemsText = document.querySelectorAll(".sidebar__links-item-text");
-    let sidebar = document.querySelector(".sidebar");
-    let logo = document.querySelector(".sidebar__logo");
-    arrows.addEventListener("click", e => {
-        if(arrows.firstElementChild.classList.contains("hidden")){
-            itemsText.forEach(text => text.style.cssText = "opacity: 0;");
-            logo.toggleAttribute("hidden")
-            sidebar.style.width = "5rem"
-            logo.parentElement.style.justifyContent = "center";
-        } else{
-            logo.toggleAttribute("hidden")
-            sidebar.style.width = "20rem"
-            itemsText.forEach(text => text.style.cssText = "opacity: 1;");
-            logo.parentElement.style.justifyContent = "space-between";
-            console.log("no hidden");
-        };
-    })
+function collapseSidebar() {
+  let arrows = document.querySelector(".sidebar__arrow");
+  let itemsText = document.querySelectorAll(".sidebar__links-item-text");
+  let sidebar = document.querySelector(".sidebar");
+  let logo = document.querySelector(".sidebar__logo");
+  arrows.addEventListener("click", e => {
+    if (arrows.classList.contains("rotate")) {
+      itemsText.forEach(text => text.style.cssText = "opacity: 0;");
+      logo.toggleAttribute("hidden")
+      sidebar.style.width = "5rem"
+      logo.parentElement.style.justifyContent = "center";
+    } else {
+      logo.toggleAttribute("hidden")
+      sidebar.style.width = ""
+      itemsText.forEach(text => text.style.cssText = "opacity: 1;");
+      logo.parentElement.style.justifyContent = "space-between";
+      console.log("no hidden");
+    };
+  })
 }
+
+function changeCurrentPage() {
+  let pages = document.querySelectorAll(".sidebar__links-item");
+  let arrow = document.querySelector(".sidebar__arrow");
+  for (let page of pages) {
+    page.addEventListener("click", e => {
+      pages.forEach(page => page.classList.remove("active"));
+      e.target.classList.add("active");
+    });
+  }
+}
+
 
 changeLinksItemColor();
-toggleSidebar();
+rotateSidebarArrow();
 collapseSidebar();
+changeCurrentPage();
